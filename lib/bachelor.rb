0f201600ab82a2d3305dd307f9251b720d4056bc
Kurt Bauer <kurtbauer55@protonmail.com>
2018-05-08 00:24:50 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2,7 +2,6 @@ require 'pry'
 def get_first_name_of_season_winner(data, season)
   # should return the first name of this seasons winner
   data[season].each do |person|
-    binding.pry
     if person["status"] == "Winner"
       return person["name"].split[0]
     end
