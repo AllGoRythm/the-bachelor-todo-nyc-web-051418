c91c8d53112b6081160acc58e3a57546eedf0749
Kurt Bauer <kurtbauer55@protonmail.com>
2018-05-08 00:47:26 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -18,7 +18,7 @@ end
 def count_contestants_by_hometown(data, hometown)
   # should return a counter of the number of contestants who are from that hometown
   data.each do |season, people|
-
+    binding.pry
   end
 end
 
