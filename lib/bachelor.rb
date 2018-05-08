4fc0e49eb30d4c22b32ac9e70714b78736ddd321
Kurt Bauer <kurtbauer55@protonmail.com>
2018-05-08 00:07:39 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,8 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  # should return the first name of this seasons winner
+  season.each do |key, value|
+    binding.pry
+  end
 end
 
 def get_contestant_name(data, occupation)
