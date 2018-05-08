39599420f30c7de0ce7d63f9794873122f89f377
Kurt Bauer <kurtbauer55@protonmail.com>
2018-05-08 00:42:19 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -12,9 +12,17 @@ def get_contestant_name(data, occupation)
   data.each do |season, people|
     people.each do |data|
       their_job = data["occupation"]
+      their_name = data["name"]
+
+      if their_job == occupation
+        return their_name
+      end
     end
   end
 
+  if condition
+    
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
