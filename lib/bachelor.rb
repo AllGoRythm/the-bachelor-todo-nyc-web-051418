6e62768967b7c7b79ad241299a619f35d33e8531
Kurt Bauer <kurtbauer55@protonmail.com>
2018-05-08 01:36:50 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -27,7 +27,11 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, people|
+    people.collect do |person|
+        return person["occupation"] if person["hometown"] == hometown
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
