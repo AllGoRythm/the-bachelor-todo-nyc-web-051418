ee57b686ffc9e586d68506ff6476c78cf4451a1c
Kurt Bauer <kurtbauer55@protonmail.com>
2018-05-08 02:23:57 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -36,7 +36,6 @@ end
 
 def get_average_age_for_season(data, season)
   collect = []
-  collect_average = collect.to_int.inject {|sum, el| sum + el}.to_f / collect.size
   data.each do |season, people|
     people.collect do |person|
         collect << person["age"]
